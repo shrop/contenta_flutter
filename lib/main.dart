1b8b0693b75925fedc1e474e850e95cfc327bf78
Mark Shropshire <mdshrops@shropnet.net>
2019-10-20 22:17:34 -0400

    Now listing recipes in a ListView from Contenta CMS' demo API

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'package:http/http.dart';
 import 'package:json_api/json_api.dart';
-import 'package:dson/dson.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,59 +11,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      home: RandomWords(),
+      home: RecipesList(),
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+class RecipesState extends State<RecipesList> {
 
-  void recipeResults() async {
+  Future<List> recipeResults() async {
+    // Get recipes from the Contenta CMS API.
+    final resultsList = new List();
     final httpClient = Client();
     final jsonApiClient = JsonApiClient(httpClient);
     final companiesUri = Uri.parse('https://dev-contentacms.pantheonsite.io/api/recipes');
     final response = await jsonApiClient.fetchCollection(companiesUri);
     httpClient.close();
 
+    // Get and return a list of Recipe titles.
     final resource = response.data.unwrap().asMap();
-
-    print('The collection page size is ${response.data.collection.length}');
-    resource.forEach((k, v) => print(resource[k].attributes["title"]));
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile
-            .divideTiles(
-              context: context,
-              tiles: tiles,
-            )
-                .toList();
-
-          return Scaffold(         // Add 6 lines from here...
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );                       // ... to here.
-        },
-      ),
-    );
+    resource.forEach((k, v) => resultsList.add(resource[k].attributes["title"]));
+    return resultsList;
   }
 
   @override
@@ -74,73 +39,50 @@ class RandomWordsState extends State<RandomWords> {
       appBar: AppBar(
         title: Text('Recipe Magazine'),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+          IconButton(icon: Icon(Icons.list), onPressed: null),
         ],
       ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      // The itemBuilder callback is called once per suggested
-      // word pairing, and places each suggestion into a ListTile
-      // row. For even rows, the function adds a ListTile row for
-      // the word pairing. For odd rows, the function adds a
-      // Divider widget to visually separate the entries. Note that
-      // the divider may be difficult to see on smaller devices.
-      itemBuilder: (BuildContext _context, int i) {
-        // Add a one-pixel-high divider widget before each row
-        // in the ListView.
-        if (i.isOdd) {
-          return Divider();
-        }
-
-        // The syntax "i ~/ 2" divides i by 2 and returns an
-        // integer result.
-        // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
-        // This calculates the actual number of word pairings
-        // in the ListView,minus the divider widgets.
-        final int index = i ~/ 2;
-        // If you've reached the end of the available word
-        // pairings...
-        if (index >= _suggestions.length) {
-          // ...then generate 10 more and add them to the
-          // suggestions list.
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      }
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: FutureBuilder(
+          future: recipeResults(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              // List Recipe titles.
+              return new ListView.builder
+                (
+                  padding: const EdgeInsets.all(16),
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext ctxt, int index) {
+                    return new Column(
+                      children: <Widget>[
+                        new ListTile(
+                          title: new Text(
+                            snapshot.data[index],
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ]
+                    );
+                  }
+                );
+            } else {
+              // Proivde a spinner while we wait on the Contenta CMS API.
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
       ),
-      trailing: Icon(   // Add the lines from here...
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {      // Add 9 lines from here...
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-            recipeResults();
-          }
-        });
-      },
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class RecipesList extends StatefulWidget {
   @override
-  RandomWordsState createState() => RandomWordsState();
+  RecipesState createState() => RecipesState();
 }
