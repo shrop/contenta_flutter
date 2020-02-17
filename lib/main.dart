301904ad2376c874647c9630a67688c6a7f8f9d8
Mark Shropshire <mdshrops@shropnet.net>
2020-02-16 21:22:44 -0500

    Setup recipes listings
    * Home/recipes list working with mock data
    * Favorites list working with mock data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:contenta_flutter/widgets/bottom_nav.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:contenta_flutter/widgets/bottom_nav.dart';
 
 void main() {
   runApp(MyApp());
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,7 +7,22 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Umami Recipes'),
       ),
-      body: Text("Hello Recipes!"),
+      body: ListView.separated(
+        separatorBuilder: (context, index) => Divider(
+          color: Colors.black,
+        ),
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Image.network(
+              'https://dev-contentacms.pantheonsite.io/sites/default/files/716259-pxhere.jpg',
+            ),
+            title: Text('Blue cheese and walnut pizza'),
+            subtitle: Text('Snack\nDifficulty: Easy Time: 14 mins'),
+            isThreeLine: true,
+          );
+        },
+      ),
     );
   }
 }
