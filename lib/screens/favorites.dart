afde778e339ff8b1c86e0be43dea896d31de161e
Mark Shropshire <mdshrops@shropnet.net>
2020-07-16 22:32:03 -0400

    Issue #84: Favorites listing can now allow opening of individual recipes

--- lib/screens/favorites.dart
+++ lib/screens/favorites.dart
@@ -3,18 +3,34 @@ import 'package:contenta_flutter/screens/recipe.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/recipe.dart';
 
 /// Gather all recipe data to display an individual recipe.
 Future<List> fetchRecipeFavs() async {
   /// Return the recipe record from Cloud Firestore.
   final databaseReference = Firestore.instance;
-  final QuerySnapshot recipes = await databaseReference
+  final QuerySnapshot favorites = await databaseReference
       .collection('recipes')
       .where('favorite', isEqualTo: true)
       .getDocuments();
 
-  final List<DocumentSnapshot> favorites = recipes.documents;
-  return favorites;
+  final List<DocumentSnapshot> favoriteRecipes = favorites.documents;
+  List recipes = [];
+
+  favoriteRecipes.forEach((e) {
+    recipes.add(Recipe(
+      e.data['id'],
+      e.data['title'],
+      e.data['difficulty'],
+      e.data['totalTime'],
+      e.data['ingredients'],
+      e.data['instructions'],
+      e.data['category'],
+      e.data['imageFileName'],
+    ));
+  });
+
+  return recipes;
 }
 
 class FavoritesPage extends StatelessWidget {
@@ -35,11 +51,11 @@ class FavoritesPage extends StatelessWidget {
             future: Future.wait([
               fetchRecipeFavs(),
             ]),
-            builder: (BuildContext context, AsyncSnapshot favorites) {
-              if (favorites.hasData) {
+            builder: (BuildContext context, AsyncSnapshot recipes) {
+              if (recipes.hasData) {
                 return ListView.builder(
                     padding: const EdgeInsets.all(16),
-                    itemCount: favorites.data[0].length,
+                    itemCount: recipes.data[0].length,
                     itemBuilder: (BuildContext ctxt, int index) {
                       return Column(children: <Widget>[
                         ListTile(
@@ -48,27 +64,25 @@ class FavoritesPage extends StatelessWidget {
                             height: 75.0,
                             child: CachedNetworkImage(
                               imageUrl: globals.API_IMAGES_URL +
-                                  favorites
-                                      .data[0][index].data['imageFileName'],
+                                  recipes.data[0][index].imageFileName,
                               errorWidget: (context, url, error) => Icon(
                                   Icons.photo_size_select_actual,
                                   size: 75.0),
                             ),
                           ),
-                          title: Text(favorites.data[0][index].data['title']),
-                          subtitle: Text(favorites
-                                  .data[0][index].data['category'] +
+                          title: Text(recipes.data[0][index].title),
+                          subtitle: Text(recipes.data[0][index].category +
                               '\nDIfficulty: ' +
-                              favorites.data[0][index].data['difficulty'] +
+                              recipes.data[0][index].difficulty +
                               ' | Time: ' +
-                              favorites.data[0][index].data['totalTimeMin']),
+                              recipes.data[0][index].totalTimeMin),
                           isThreeLine: true,
                           onTap: () {
                             Navigator.push(
                               context,
                               MaterialPageRoute(
                                   builder: (context) => RecipePage(
-                                      recipe: favorites.data[0][index])),
+                                      recipe: recipes.data[0][index])),
                             );
                           },
                         )
